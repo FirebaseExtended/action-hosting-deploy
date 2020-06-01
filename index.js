d4b8384cc817f7fec0f2bcc51ca45789438e7d8b
Jeff Huleatt <jhuleatt@google.com>
2020-06-01 20:06:55 +0000

    another Prettier commit

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ async function run(github, context) {
                 summary: `[${result.url}](${result.url})`
             }
         });
-    } catch (e) {
-        setFailed(e.message);
+	} catch (e) {
+		setFailed(e.message);
 
         await finish({
             conclusion: 'failure',
@@ -141,49 +141,49 @@ async function createCheck(github, context) {
 // create a PR comment, or update one if it already exists
 async function postOrUpdateComment(github, context, commentMarkdown) {
     const commentInfo = {
-            ...context.repo,
-            issue_number: context.issue.number
-        };
+		...context.repo,
+		issue_number: context.issue.number
+	};
 
-        const comment = {
-            ...commentInfo,
-            body: commentMarkdown + '\n\n<sub>firebase-hosting-preview-action</sub>'
-        };
+	const comment = {
+		...commentInfo,
+		body: commentMarkdown + '\n\n<sub>firebase-hosting-preview-action</sub>'
+	};
 
-        startGroup(`Updating PR comment`);
-        let commentId;
-        try {
-            const comments = (await github.issues.listComments(commentInfo)).data;
-            for (let i = comments.length; i--;) {
-                const c = comments[i];
-                if (c.user.type === 'Bot' && /<sub>[\s\n]*firebase-hosting-preview-action/.test(c.body)) {
-                    commentId = c.id;
-                    break;
-                }
-            }
-        }
-        catch (e) {
-            console.log('Error checking for previous comments: ' + e.message);
-        }
+	startGroup(`Updating PR comment`);
+	let commentId;
+	try {
+		const comments = (await github.issues.listComments(commentInfo)).data;
+		for (let i=comments.length; i--; ) {
+			const c = comments[i];
+			if (c.user.type === 'Bot' && /<sub>[\s\n]*firebase-hosting-preview-action/.test(c.body)) {
+				commentId = c.id;
+				break;
+			}
+		}
+	}
+	catch (e) {
+		console.log('Error checking for previous comments: ' + e.message);
+	}
 
-        if (commentId) {
-            try {
-                await github.issues.updateComment({
-                    ...context.repo,
-                    comment_id: commentId,
-                    body: comment.body
-                });
-            }
-            catch (e) {
-                commentId = null;
-            }
-        }
+	if (commentId) {
+		try {
+			await github.issues.updateComment({
+				...context.repo,
+				comment_id: commentId,
+				body: comment.body
+			});
+		}
+		catch (e) {
+			commentId = null;
+		}
+	}
 
-        if (!commentId) {
-            try {
-                await github.issues.createComment(comment);
-            } catch (e) {
-                console.log(`Error creating comment: ${e.message}`);
+	if (!commentId) {
+		try {
+			await github.issues.createComment(comment);
+		} catch (e) {
+			console.log(`Error creating comment: ${e.message}`);
         }
     }
     endGroup();
