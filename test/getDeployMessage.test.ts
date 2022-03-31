f9afb0ef1cdbb6b6b583f7e41d2cec718877469e
Halvard Mørstad <halvard@simplyundoable.com>
2022-03-31 11:09:09 +0100

    format

--- test/getDeployMessage.test.ts
+++ test/getDeployMessage.test.ts
@@ -9,13 +9,13 @@ const message = `head commit message that is longer than 255 characters will be
 automatically reduced and we do not want that potentially. But we also want 
 to check for multiline support so here is a rather long rant which hopefully 
 one day will reach 255 characters (hopefully).`;
-const context = {
+const context = ({
   payload: {
     head_commit: {
       message,
     },
   },
-} as unknown as Context;
+} as unknown) as Context;
 
 describe("getDeployMessage", () => {
   it("using false", () => {
@@ -31,11 +31,13 @@ describe("getDeployMessage", () => {
   });
 
   it("return no head commit", () => {
-    expect(getDeployMessage("true", {} as unknown as Context)).toBeUndefined();
+    expect(
+      getDeployMessage("true", ({} as unknown) as Context)
+    ).toBeUndefined();
   });
 
   it("provided message is capped", () => {
-    expect(getDeployMessage(message, {} as unknown as Context)).toEqual(
+    expect(getDeployMessage(message, ({} as unknown) as Context)).toEqual(
       messageCapped
     );
   });
