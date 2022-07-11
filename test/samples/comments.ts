c1743a091815bd6b9e1cc0883b40a6c054918d0e
KKimj <kkimj@hanyang.ac.kr>
2022-07-11 18:49:13 +0900

    Update with unit-tests

--- test/samples/comments.ts
+++ test/samples/comments.ts
@@ -5,6 +5,8 @@ export const multiSiteComment = `Visit the preview URL for this PR (updated for 
 
 <sub>(expires Tue, 27 Oct 2020 21:32:57 GMT)</sub>
 
+<sub>(target target1)</sub>
+
 <sub>🔥 via [Firebase Hosting GitHub Action](https://github.com/marketplace/actions/deploy-to-firebase-hosting) 🌎</sub>
 
 <sub>Sign: 980f04126fb629deaadace7d6ee8a0628942e3d3</sub>`.trim();
@@ -14,6 +16,8 @@ export const singleSiteComment = `Visit the preview URL for this PR (updated for
 [https://action-hosting-deploy-demo--singlesite-test-jl98rmie.web.app](https://action-hosting-deploy-demo--singlesite-test-jl98rmie.web.app)
 
 <sub>(expires Tue, 27 Oct 2020 21:32:57 GMT)</sub>
+
+<sub>(target default)</sub>
 
 <sub>🔥 via [Firebase Hosting GitHub Action](https://github.com/marketplace/actions/deploy-to-firebase-hosting) 🌎</sub>
 
